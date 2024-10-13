8aab5cc4988ae5230addb126ee6bad32fdfabd5f
Afian Anwar <afian.login@gmail.com>
2024-10-13 09:12:21 +0800

    Removed backend

--- backend/routes/placeDetails.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getPlaceDetails } = require("../controllers/placeController");
-const router = express.Router();
-
-router.get("/", getPlaceDetails);
-
-module.exports = router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { APIProvider } from "@vis.gl/react-google-maps";
 import Map from "./components/Map";
 import LocationSearchPanel from "./components/LocationSearchPanel";
-import axios from "axios";
 import "./App.scss";
 
 const App = () => {
@@ -10,23 +9,10 @@ const App = () => {
 
   const handlePlaceSelection = async (placeResult) => {
     try {
-      const locationName = placeResult.name;
-      const response = await axios.get(
-        `${process.env.REACT_APP_BACKEND_URI}/place-details`,
-        {
-          params: { locationName },
-        }
-      );
 
-      const details = response.data;
-
-      if (details) {
+      if (placeResult) {
         setSelectedPlace({
-          name: details.name,
-          rating: details.rating,
-          reviewsCount: details.reviewsCount,
-          category: details.category,
-          imageUrl: details.imageUrl,
+          name: placeResult.name,
           address: placeResult.formatted_address,
           latitude: placeResult.geometry?.location?.lat(),
           longitude: placeResult.geometry?.location?.lng(),
