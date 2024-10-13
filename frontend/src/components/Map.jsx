0067bef1a116266d74558007bcbd1f5450c5bd04
Afian Anwar <afian.login@gmail.com>
2024-10-13 09:01:19 +0800

    Initial commit

---
+++ frontend/src/components/Map.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { Map as GMap, useMap } from "@vis.gl/react-google-maps";
+import Marker from "./Marker";
+
+const Map = ({ place }) => {
+  const mapInstance = useMap();
+
+  useEffect(() => {
+    if (place && mapInstance) {
+      const { latitude, longitude } = place;
+      mapInstance.panTo({ lat: latitude, lng: longitude });
+    }
+  }, [place, mapInstance]);
+
+  return (
+    <GMap
+      defaultCenter={{ lat: 49.2569501547411, lng: -123.11058970045666 }}
+      defaultZoom={16}
+      disableDefaultUI
+    >
+      {place && <Marker place={place} />}
+    </GMap>
+  );
+};
+
+export default Map;
